95299a51cb03ef3fb8e650a814cfe799a655948a
Andriyan Ivanov <andriyan.ivanov@gmail.com>
2025-12-01 23:45:15 +0200

    feat: use tailwind for styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,14 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
   site: process.env.SITE_URL || "http://localhost:4321",
   integrations: [mdx(), sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
